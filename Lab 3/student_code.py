a4da2157720b91eecedc635d97b5e3eb8afe4dbe
arturofonseca <arturofonseca2025@u.northwestern.edu>
2024-09-18 21:06:42 -0500

    Yes

--- Lab 3/student_code.py
+++ Lab 3/student_code.py
@@ -1,9 +1,8 @@
-import copy
-
 class Pos:
     def __init__(self, y: int, x: int) -> None:
         self.y = y
         self.x = x
+
 
 # graphic print of board, feel free to use, or not
 def print_board(array):
@@ -35,13 +34,15 @@ def print_board(array):
     )
     print("------------")
 
+
 def arrayToBoard(array: list) -> list:
     board = []
     for i in range(3):
         board.append([])
         for j in range(3):
-            board[i].append(array[3*i + j])
+            board[i].append(array[3 * i + j])
     return board
+
 
 def boardToArray(board: list) -> list:
     array = []
@@ -49,33 +50,39 @@ def boardToArray(board: list) -> list:
         array += row
     return array
 
+
 def hForNode(board: list, pos: Pos) -> int:
     num = board[pos.y][pos.x]
     if num == 0:
         return 0
-    y = ((num - 1) - ((num - 1) % 3))//3
+    y = ((num - 1) - ((num - 1) % 3)) // 3
     x = (num - 1) % 3
     goal = Pos(y, x)
     dy = abs(pos.y - goal.y)
     dx = abs(pos.x - goal.x)
     return dy + dx
 
+
 def h(board: list) -> int:
     cost = 0
     for y in range(3):
         for x in range(3):
-            cost += hForNode(board, Pos(y,x))
+            cost += hForNode(board, Pos(y, x))
     return cost
+
 
 def copyBoard(board: list) -> list:
     array = boardToArray(board)
     return arrayToBoard(array)
+
 
 def findZero(board: list) -> Pos:
     for y in range(3):
         for x in range(3):
             if board[y][x] == 0:
                 return Pos(y, x)
+    return Pos(0, 0)
+
 
 def genChildBoard(board: list, zeroPos: Pos, child: Pos) -> list:
     copy = copyBoard(board)
@@ -83,8 +90,9 @@ def genChildBoard(board: list, zeroPos: Pos, child: Pos) -> list:
     copy[child.y][child.x] = 0
     return copy
 
+
 def findChildren(board: list, zeroPos: Pos) -> list:
-    childPos_sequence= []
+    childPos_sequence = []
     if zeroPos.x < 2:
         childPos_sequence.append((Pos(zeroPos.y, zeroPos.x + 1), 1))
     if zeroPos.y < 2:
@@ -100,6 +108,7 @@ def findChildren(board: list, zeroPos: Pos) -> list:
         childBoard_sequence.append((childBoard, child[1]))
     return childBoard_sequence
 
+
 def findMinIndex(frontier: list, frontierfN: list) -> int:
     maxIndex = 0
     maxFrontier = []
@@ -111,19 +120,22 @@ def findMinIndex(frontier: list, frontierfN: list) -> int:
                 maxIndex = i
     return maxIndex
 
+
 def checkSeen(board: list, seen: list):
     array = boardToArray(board)
     if array in seen:
         return True
     else:
         return False
-    
-def findDupIndexes(frontier: list, current: list) -> None:
+
+
+def findDupIndexes(frontier: list, current: list) -> list:
     dupIndexes = []
     for i in range(len(frontier)):
         if frontier[i] == current:
             dupIndexes.append(i)
     return dupIndexes
+
 
 # astar search
 def astar(array: list):
